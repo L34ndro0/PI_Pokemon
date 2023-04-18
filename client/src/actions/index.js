1ed82d55b6b1b973b3e4714bbac0c7c6be9f84a3
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-04-17 22:48:16 -0300

    deploy

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -31,7 +31,7 @@ export function postPokemon (payload) {
 export function getDetail(idPokemon){
     return async function (dispatch) {
         try {
-            const json = await axios(`/${idPokemon}`)
+            const json = await axios(`pokemons/${idPokemon}`)
             return dispatch({
                 type: GET_DETAILS,
                 payload: json.data
