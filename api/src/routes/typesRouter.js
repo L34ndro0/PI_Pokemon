8180269ccb12bb87533e48b51893f1b9056297c7
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-04-17 21:55:29 -0300

    deploy

--- api/src/routes/typesRouter.js
+++
@@ -1,9 +0,0 @@
-const getAllTypes = require('../controllers/getAllTypes')
-
-const typesRouter = require('express').Router()
-
-typesRouter.get('/types', async (req,res) => {
-    res.status(200).send( await getAllTypes())
-})
-
-module.exports = typesRouter
